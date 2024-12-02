5bd6025c6d15e773fb84becc86416648ff497b12
Bagus Satrio <115222053+bagussatt@users.noreply.github.com>
2024-12-02 11:54:36 +0700

    Add Controller, Model, and Migration

---
+++ app/Http/Controllers/DokumenController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\dokumen;
+use Illuminate\Http\Request;
+
+class DokumenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(dokumen $dokumen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(dokumen $dokumen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, dokumen $dokumen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(dokumen $dokumen)
+    {
+        //
+    }
+}
