8775a1f634097c4a91725e5a458a366b03c61de8
Bagus Satrio <115222053+bagussatt@users.noreply.github.com>
2024-12-11 14:19:56 +0700

    `Refactor API routes to use middleware for role-based access control`

--- routes/api.php
+++ routes/api.php
@@ -14,117 +14,109 @@ Route::post('/login', [AuthController::class, 'login'])->name('logins');
 Route::post('/logout', [AuthController::class, 'logout']);
 
 
-Route::prefix('nr')->group(function(){
+// Route::prefix('nr')->group(function(){
+//         // Routes untuk Users
+//     Route::get('/users', [UsersController::class, 'index']);
+//     Route::post('/users/post', [UsersController::class, 'store']);
+//     Route::get('/users/{id}', [UsersController::class, 'show']);
+//     Route::put('/users/update/{id}', [UsersController::class, 'update']);
+//     Route::delete('/users/delete/{id}', [UsersController::class, 'destroy']);
+
+//     // Routes untuk Pengajuan
+//     Route::get('/submission', [PengajuanController::class, 'index']);
+//     Route::post('/submission/post', [PengajuanController::class, 'store']);
+//     Route::get('/submission/{id}', [PengajuanController::class, 'show']);
+//     Route::put('/submission/update/{id}', [PengajuanController::class, 'update']);
+//     Route::delete('/submission/delete/{id}', [PengajuanController::class, 'destroy']);
+
+//     // Routes untuk Jadwal
+//     Route::get('schedules', [JadwalController::class, 'index']);
+//     Route::post('schedules/post', [JadwalController::class, 'store']);
+//     Route::get('schedules/{id}', [JadwalController::class, 'show']);
+//     Route::put('schedules/update/{id}', [JadwalController::class, 'update']);
+//     Route::delete('schedules/delete/{id}', [JadwalController::class, 'destroy']);
+
+//     // Routes untuk Notifikasi
+//     Route::get('notification', [NotifikasiController::class, 'index']);
+//     Route::post('notification/post', [NotifikasiController::class, 'store']);
+//     Route::get('notification/{id}', [NotifikasiController::class, 'show']);
+//     Route::put('notification/update/{id}', [NotifikasiController::class, 'update']);
+//     Route::delete('notification/delete/{id}', [NotifikasiController::class, 'destroy']);
+
+//     // Routes untuk Dokumen
+//     Route::get('document', [DokumenController::class, 'index']);
+//     Route::post('document/post', [DokumenController::class, 'store']);
+//     Route::delete('document/delete/{id}', [DokumenController::class, 'destroy']);
+//     });
+
+Route::middleware(['auth:sanctum'])->group(function() {
+
+    // Routes untuk Super Admin
+    Route::prefix('super')->group(function() {
         // Routes untuk Users
-    Route::get('/users', [UsersController::class, 'index']);
-    Route::post('/users/post', [UsersController::class, 'store']);
-    Route::get('/users/{id}', [UsersController::class, 'show']);
-    Route::put('/users/update/{id}', [UsersController::class, 'update']);
-    Route::delete('/users/delete/{id}', [UsersController::class, 'destroy']);
-
-    // Routes untuk Pengajuan
-    Route::get('/submission', [PengajuanController::class, 'index']);
-    Route::post('/submission/post', [PengajuanController::class, 'store']);
-    Route::get('/submission/{id}', [PengajuanController::class, 'show']);
-    Route::put('/submission/update/{id}', [PengajuanController::class, 'update']);
-    Route::delete('/submission/delete/{id}', [PengajuanController::class, 'destroy']);
-
-    // Routes untuk Jadwal
-    Route::get('schedules', [JadwalController::class, 'index']);
-    Route::post('schedules/post', [JadwalController::class, 'store']);
-    Route::get('schedules/{id}', [JadwalController::class, 'show']);
-    Route::put('schedules/update/{id}', [JadwalController::class, 'update']);
-    Route::delete('schedules/delete/{id}', [JadwalController::class, 'destroy']);
-
-    // Routes untuk Notifikasi
-    Route::get('notification', [NotifikasiController::class, 'index']);
-    Route::post('notification/post', [NotifikasiController::class, 'store']);
-    Route::get('notification/{id}', [NotifikasiController::class, 'show']);
-    Route::put('notification/update/{id}', [NotifikasiController::class, 'update']);
-    Route::delete('notification/delete/{id}', [NotifikasiController::class, 'destroy']);
-
-    // Routes untuk Dokumen
-    Route::get('document', [DokumenController::class, 'index']);
-    Route::post('document/post', [DokumenController::class, 'store']);
-    Route::delete('document/delete/{id}', [DokumenController::class, 'destroy']);
-    });
-
-
-//penulisan route http://127.0.0.1:8000/api/super/(routenya contoh = users)
-Route::prefix('super')->name('super_admin.')->group(function(){
-    Route::middleware('can:manage')->group(function(){
-        // Routes untuk Users
-    Route::get('/users', [UsersController::class, 'index']);
-    Route::post('/users/post', [UsersController::class, 'store']);
-    Route::get('/users/{user}', [UsersController::class, 'show']);
-    Route::put('/users/{user}', [UsersController::class, 'update']);
-    Route::delete('/users/{user}', [UsersController::class, 'destroy']);
-
-    // Routes untuk Pengajuan
-    Route::get('/submission', [PengajuanController::class, 'index']);
-    Route::post('/submission/post', [PengajuanController::class, 'store']);
-    Route::get('/submission/{id}', [PengajuanController::class, 'show']);
-    Route::put('/submission/{id}', [PengajuanController::class, 'update']);
-    Route::delete('/submission/{id}', [PengajuanController::class, 'destroy']);
-
-    // Routes untuk Jadwal
-    Route::get('schedules', [JadwalController::class, 'index']);
-    Route::post('schedules/post', [JadwalController::class, 'store']);
-    Route::get('schedules/{id}', [JadwalController::class, 'show']);
-    Route::put('schedules/{id}', [JadwalController::class, 'update']);
-    Route::delete('schedules/{id}', [JadwalController::class, 'destroy']);
-
-    // Routes untuk Notifikasi
-    Route::get('notification', [NotifikasiController::class, 'index']);
-    Route::post('notification/post', [NotifikasiController::class, 'store']);
-    Route::get('notification/{id}', [NotifikasiController::class, 'show']);
-    Route::put('notification/{id}', [NotifikasiController::class, 'update']);
-    Route::delete('notification/{id}', [NotifikasiController::class, 'destroy']);
-
-    // Routes untuk Dokumen
-    Route::get('document', [DokumenController::class, 'index']);
-    Route::post('document/post', [DokumenController::class, 'store']);
-    Route::delete('document/{id}', [DokumenController::class, 'destroy']);
-    });
-});
-//penulisan route http://127.0.0.1:8000/api/admin/(route)
-Route::prefix('admin')->name('admin.')->group(function(){
-    Route::middleware('can:manage')->group(function(){
-         // Routes untuk Users
-         Route::get('/users/{user}', [UsersController::class, 'show']);
-         Route::put('/users/{user}', [UsersController::class, 'update']);
-
-         // Routes untuk Pengajuan
-         Route::get('/submission', [PengajuanController::class, 'index']);
-         Route::post('/submission/post', [PengajuanController::class, 'store']);
-         Route::get('/submission/{id}', [PengajuanController::class, 'show']);
-         Route::put('/submission/{id}', [PengajuanController::class, 'update']);
-
-         // Routes untuk Jadwal
-         Route::get('schedules', [JadwalController::class, 'index']);
-         Route::get('schedules/{id}', [JadwalController::class, 'show']);
-         Route::put('schedules/{id}', [JadwalController::class, 'update']);
-
-         // Routes untuk Notifikasi
-         Route::get('notification', [NotifikasiController::class, 'index']);
-         Route::get('notification/{id}', [NotifikasiController::class, 'show']);
-         Route::put('notification/{id}', [NotifikasiController::class, 'update']);
-});
-//penulisan route http://127.0.0.1:8000/api/user/(route)
-Route::prefix('user')->name('user.')->group(function(){
-    Route::middleware('can:make') -> group(function(){
-        Route::get('schedules', [JadwalController::class, 'index']);
-        Route::post('schedules/post', [JadwalController::class, 'store']);
-        Route::get('schedules/{id}', [JadwalController::class, 'show']);
+        Route::get('/users', [UsersController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::post('/users/post', [UsersController::class, 'store'])->middleware('role:super_admin');
+        Route::get('/users/{user}', [UsersController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/users/{user}', [UsersController::class, 'update'])->middleware('role:super_admin|manage');
+        Route::delete('/users/{user}', [UsersController::class, 'destroy'])->middleware('role:super_admin');
 
         // Routes untuk Pengajuan
-        Route::get('/submission', [PengajuanController::class, 'index']);
-        Route::post('/submission/post', [PengajuanController::class, 'store']);
-        Route::get('/submission/{id}', [PengajuanController::class, 'show']);
-      // routes dokumen
-        Route::get('document', [DokumenController::class, 'index']); // Menampilkan semua dokumen
-        Route::post('document/post', [DokumenController::class, 'store']); // Menyimpan dokumen baru
+        Route::get('/submission', [PengajuanController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::post('/submission/post', [PengajuanController::class, 'store'])->middleware('role:super_admin');
+        Route::get('/submission/{id}', [PengajuanController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/submission/{id}', [PengajuanController::class, 'update'])->middleware('role:super_admin|manage');
+        Route::delete('/submission/{id}', [PengajuanController::class, 'destroy'])->middleware('role:super_admin');
+
+        // Routes untuk Jadwal
+        Route::get('/schedules', [JadwalController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::post('/schedules/post', [JadwalController::class, 'store'])->middleware('role:super_admin|manage');
+        Route::get('/schedules/{id}', [JadwalController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/schedules/{id}', [JadwalController::class, 'update'])->middleware('role:super_admin|manage');
+        Route::delete('/schedules/{id}', [JadwalController::class, 'destroy'])->middleware('role:super_admin');
+
+        // Routes untuk Notifikasi
+        Route::get('/notification', [NotifikasiController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::post('/notification/post', [NotifikasiController::class, 'store'])->middleware('role:super_admin|manage');
+        Route::get('/notification/{id}', [NotifikasiController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/notification/{id}', [NotifikasiController::class, 'update'])->middleware('role:super_admin|manage');
+        Route::delete('/notification/{id}', [NotifikasiController::class, 'destroy'])->middleware('role:super_admin');
+
+        // Routes untuk Dokumen
+        Route::get('/document', [DokumenController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::post('/document/post', [DokumenController::class, 'store'])->middleware('role:super_admin|manage');
+        Route::delete('/document/{id}', [DokumenController::class, 'destroy'])->middleware('role:super_admin');
     });
-});});
 
+    // Admin Routes
+    Route::prefix('admin')->middleware('can:manage')->group(function() {
+        Route::get('/users/{user}', [UsersController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/users/{user}', [UsersController::class, 'update'])->middleware('role:super_admin|manage');
 
+        Route::get('/submission', [PengajuanController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::post('/submission/post', [PengajuanController::class, 'store'])->middleware('role:super_admin|manage');
+        Route::get('/submission/{id}', [PengajuanController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/submission/{id}', [PengajuanController::class, 'update'])->middleware('role:super_admin|manage');
+
+        Route::get('/schedules', [JadwalController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::get('/schedules/{id}', [JadwalController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/schedules/{id}', [JadwalController::class, 'update'])->middleware('role:super_admin|manage');
+
+        Route::get('/notification', [NotifikasiController::class, 'index'])->middleware('role:super_admin|manage');
+        Route::get('/notification/{id}', [NotifikasiController::class, 'show'])->middleware('role:super_admin|manage');
+        Route::put('/notification/{id}', [NotifikasiController::class, 'update'])->middleware('role:super_admin|manage');
+    });
+
+    // User Routes
+    Route::prefix('user')->middleware('can:make')->group(function() {
+        Route::get('/schedules', [JadwalController::class, 'index'])->middleware('role:user|make');
+        Route::post('/schedules/post', [JadwalController::class, 'store'])->middleware('role:user|make');
+        Route::get('/schedules/{id}', [JadwalController::class, 'show'])->middleware('role:user|make');
+
+        Route::get('/submission', [PengajuanController::class, 'index'])->middleware('role:user|make');
+        Route::post('/submission/post', [PengajuanController::class, 'store'])->middleware('role:user|make');
+        Route::get('/submission/{id}', [PengajuanController::class, 'show'])->middleware('role:user|make');
+
+        Route::get('/document', [DokumenController::class, 'index'])->middleware('role:user|make');
+        Route::post('/document/post', [DokumenController::class, 'store'])->middleware('role:user|make');
+    });
+});
