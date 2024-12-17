f32e0590c791b68968477ae252752b9f8bdc0609
Bagus Satrio <115222053+bagussatt@users.noreply.github.com>
2024-12-17 11:33:36 +0700

    add new sktm api

--- routes/api.php
+++ routes/api.php
@@ -20,23 +20,25 @@ use App\Http\Controllers\API\SubsktmController;
 use App\Http\Controllers\API\SuratUsahaController;
 
 
-
+// spesial api
 Route::post('/login', [AuthController::class, 'login'])->name('logins');
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/role/{roleName}', [RoleController::class, 'getUsersByRole']);
 Route::put('/{userId}/role', [RoleController::class, 'updateUserRole']);
+
+
 Route::get('/submission', [PengajuanController::class, 'index']);
 Route::get('/submission/admin', [PengajuanController::class, 'indexAdmin']);
 Route::get('/submission/user', [PengajuanController::class, 'indexUser']);
 Route::get('/submission/all', [PengajuanController::class, 'indexAll']);
-Route::post('/sktm', [SKTMController::class, 'store']);
-Route::get('supSktm', [PendukungSktmController::class, 'index']);
-Route::post('supSktm', [PendukungSktmController::class, 'store']);
-Route::get('supSktm/{id}', [PendukungSktmController::class, 'show']);
-Route::put('supSktm/{id}', [PendukungSktmController::class, 'update']);
-Route::delete('supSktm/{id}', [PendukungSktmController::class, 'destroy']);
-Route::get('docSktm', [DokumenSktmController::class, 'index']);
-Route::post('docSktm', [DokumenSktmController::class, 'store']);
+// Route::post('/sktm', [SKTMController::class, 'store']);
+// Route::get('supSktm', [PendukungSktmController::class, 'index']);
+// Route::post('supSktm', [PendukungSktmController::class, 'store']);
+// Route::get('supSktm/{id}', [PendukungSktmController::class, 'show']);
+// Route::put('supSktm/{id}', [PendukungSktmController::class, 'update']);
+// Route::delete('supSktm/{id}', [PendukungSktmController::class, 'destroy']);
+// Route::get('docSktm', [DokumenSktmController::class, 'index']);
+// Route::post('docSktm', [DokumenSktmController::class, 'store']);
 
 Route::post('/document/post', [DokumenController::class, 'store']);
 
@@ -69,41 +71,12 @@ Route::get('statistik', [StatistikController::class, 'getStatistik']);
 Route::get('montrep', [StatistikController::class, 'getStatistikPerBulan']);
 Route::post('/sendEmail', [NotifikasiController::class, 'sendConfirmationEmail']);
 Route::apiResource('subsktms', SubsktmController::class);
+Route::get('sktm',[SubsktmController::class,'index']);
+Route::post('/sktm', [SubsktmController::class, 'store']);
+Route::get('/sktm/{id}', [SubsktmController::class, 'show']);
+Route::put('/sktm/{id}', [SubsktmController::class, 'update']);
+Route::delete('/sktm/{id}', [SubsktmController::class, 'destroy']);
 
-// Route::prefix('nr')->group(function(){
-//         // Routes untuk Users
-//     Route::get('/users', [UsersController::class, 'index']);
-//     Route::post('/users/post', [UsersController::class, 'store']);
-//     Route::get('/users/{id}', [UsersController::class, 'show']);
-//     Route::put('/users/update/{id}', [UsersController::class, 'update']);
-//     Route::delete('/users/delete/{id}', [UsersController::class, 'destroy']);
-
-//     // Routes untuk Pengajuan
-//     Route::get('/submission', [PengajuanController::class, 'index']);
-//     Route::post('/submission/post', [PengajuanController::class, 'store']);
-//     Route::get('/submission/{id}', [PengajuanController::class, 'show']);
-//     Route::put('/submission/update/{id}', [PengajuanController::class, 'update']);
-//     Route::delete('/submission/delete/{id}', [PengajuanController::class, 'destroy']);
-
-//     // Routes untuk Jadwal
-//     Route::get('schedules', [JadwalController::class, 'index']);
-//     Route::post('schedules/post', [JadwalController::class, 'store']);
-//     Route::get('schedules/{id}', [JadwalController::class, 'show']);
-//     Route::put('schedules/update/{id}', [JadwalController::class, 'update']);
-//     Route::delete('schedules/delete/{id}', [JadwalController::class, 'destroy']);
-
-//     // Routes untuk Notifikasi
-//     Route::get('notification', [NotifikasiController::class, 'index']);
-//     Route::post('notification/post', [NotifikasiController::class, 'store']);
-//     Route::get('notification/{id}', [NotifikasiController::class, 'show']);
-//     Route::put('notification/update/{id}', [NotifikasiController::class, 'update']);
-//     Route::delete('notification/delete/{id}', [NotifikasiController::class, 'destroy']);
-
-//     // Routes untuk Dokumen
-//     Route::get('document', [DokumenController::class, 'index']);
-//     Route::post('document/post', [DokumenController::class, 'store']);
-//     Route::delete('document/delete/{id}', [DokumenController::class, 'destroy']);
-//     });
 
 Route::middleware(['auth:sanctum'])->group(function() {
 
@@ -176,41 +149,47 @@ Route::put('/{userId}/role', [RoleController::class, 'updateUserRole'])->middlew
     Route::prefix('user')->middleware('can:make')->group(function() {
         Route::get('docSktm', [DokumenSktmController::class, 'index'])->middleware('role:user|make');
         Route::post('docSktm', [DokumenSktmController::class, 'store'])->middleware('role:user|make');
+
         Route::get('/usaha', [SuratUsahaController::class, 'index'])->middleware('role:user|make');
         Route::post('/usaha', [SuratUsahaController::class, 'store'])->middleware('role:user|make');
         Route::get('/usaha/{id}', [SuratUsahaController::class, 'show'])->middleware('role:user|make');
         Route::put('/usaha/{id}', [SuratUsahaController::class, 'update'])->middleware('role:user|make');
         Route::delete('/usaha/{id}', [SuratUsahaController::class, 'destroy'])->middleware('role:user|make');
+
         Route::get('supusaha', [SupportUsahaController::class, 'index'])->middleware('role:user|make');
         Route::get('supusaha/{id}', [SupportUsahaController::class, 'show'])->middleware('role:user|make');
         Route::post('supusaha', [SupportUsahaController::class, 'store'])->middleware('role:user|make');
         Route::put('supusaha/{id}', [SupportUsahaController::class, 'update'])->middleware('role:user|make');
         Route::delete('supusaha/{id}', [SupportUsahaController::class, 'destroy'])->middleware('role:user|make');
+
         Route::post('/skck', [SkckController::class, 'store'])->middleware('role:user|make');
         Route::get('/skck', [SkckController::class, 'index'])->middleware('role:user|make');
         Route::get('/skck/{id}', [SkckController::class, 'show'])->middleware('role:user|make');
         Route::put('/skck/{id}', [SkckController::class, 'update'])->middleware('role:user|make');
         Route::delete('/skck/{id}', [SkckController::class, 'destroy'])->middleware('role:user|make');
+
         Route::post('/domisili', [DomisiliController::class, 'store'])->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
         Route::get('/domisili', [DomisiliController::class, 'index'])->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
         Route::get('/domisili/{id}', [DomisiliController::class, 'show'])->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
         Route::put('/domisili/{id}', [DomisiliController::class, 'update'])->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
         Route::delete('/domisili/{id}', [DomisiliController::class, 'destroy'])->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
-        Route::post('/sktm', [SKTMController::class, 'store'])->middleware('role:user|make')->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
-        // Route::resource('/sktm', [SKTMController::class])->middleware('role:user|make');
-        Route::get('supSktm', [PendukungSktmController::class, 'index'])->middleware('role:user|make');
-        Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
-        Route::get('supSktm/{id}', [PendukungSktmController::class, 'show'])->middleware('role:user|make');
-        Route::put('supSktm/{id}', [PendukungSktmController::class, 'update'])->middleware('role:user|make');
-        Route::delete('supSktm/{id}', [PendukungSktmController::class, 'destroy'])->middleware('role:user|make');
+        Route::get('sktm',[SubsktmController::class,'index'])->middleware('role:user|make');
+        Route::post('/sktm', [SubsktmController::class, 'store'])->middleware('role:user|make');
+        Route::get('/sktm/{id}', [SubsktmController::class, 'show'])->middleware('role:user|make');
+        Route::put('/sktm/{id}', [SubsktmController::class, 'update'])->middleware('role:user|make');
+        Route::delete('/sktm/{id}', [SubsktmController::class, 'destroy'])->middleware('role:user|make');
+
+        // Route::post('/sktm', [SKTMController::class, 'store'])->middleware('role:user|make');
+
+        // Route::get('supSktm', [PendukungSktmController::class, 'index'])->middleware('role:user|make');
+        // Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+        // Route::get('supSktm/{id}', [PendukungSktmController::class, 'show'])->middleware('role:user|make');
+        // Route::put('supSktm/{id}', [PendukungSktmController::class, 'update'])->middleware('role:user|make');
+        // Route::delete('supSktm/{id}', [PendukungSktmController::class, 'destroy'])->middleware('role:user|make');
+
         Route::get('/users', [UsersController::class, 'index'])->middleware('role:user|make');
         Route::put('/users/{user}', [UsersController::class, 'update'])->middleware('role:user|make');
+
         Route::get('/schedules', [JadwalController::class, 'index'])->middleware('role:user|make');
         Route::post('/schedules/post', [JadwalController::class, 'store'])->middleware('role:user|make');
         Route::get('/schedules/{id}', [JadwalController::class, 'show'])->middleware('role:user|make');
@@ -222,4 +201,91 @@ Route::put('/{userId}/role', [RoleController::class, 'updateUserRole'])->middlew
         Route::get('/document', [DokumenController::class, 'index'])->middleware('role:user|make');
         Route::post('/document/post', [DokumenController::class, 'store'])->middleware('role:user|make');
     });
+
+
 });
+// Route::prefix('nr')->group(function(){
+//         // Routes untuk Users
+//     Route::get('/users', [UsersController::class, 'index']);
+//     Route::post('/users/post', [UsersController::class, 'store']);
+//     Route::get('/users/{id}', [UsersController::class, 'show']);
+//     Route::put('/users/update/{id}', [UsersController::class, 'update']);
+//     Route::delete('/users/delete/{id}', [UsersController::class, 'destroy']);
+
+//     // Routes untuk Pengajuan
+//     Route::get('/submission', [PengajuanController::class, 'index']);
+//     Route::post('/submission/post', [PengajuanController::class, 'store']);
+//     Route::get('/submission/{id}', [PengajuanController::class, 'show']);
+//     Route::put('/submission/update/{id}', [PengajuanController::class, 'update']);
+//     Route::delete('/submission/delete/{id}', [PengajuanController::class, 'destroy']);
+
+//     // Routes untuk Jadwal
+//     Route::get('schedules', [JadwalController::class, 'index']);
+//     Route::post('schedules/post', [JadwalController::class, 'store']);
+//     Route::get('schedules/{id}', [JadwalController::class, 'show']);
+//     Route::put('schedules/update/{id}', [JadwalController::class, 'update']);
+//     Route::delete('schedules/delete/{id}', [JadwalController::class, 'destroy']);
+
+//     // Routes untuk Notifikasi
+//     Route::get('notification', [NotifikasiController::class, 'index']);
+//     Route::post('notification/post', [NotifikasiController::class, 'store']);
+//     Route::get('notification/{id}', [NotifikasiController::class, 'show']);
+//     Route::put('notification/update/{id}', [NotifikasiController::class, 'update']);
+//     Route::delete('notification/delete/{id}', [NotifikasiController::class, 'destroy']);
+
+//     // Routes untuk Dokumen
+//     Route::get('document', [DokumenController::class, 'index']);
+//     Route::post('document/post', [DokumenController::class, 'store']);
+//     Route::delete('document/delete/{id}', [DokumenController::class, 'destroy']);
+//     });
+
+
+// Route::prefix('user')->middleware('can:make')->group(function() {
+    //     Route::get('docSktm', [DokumenSktmController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('docSktm', [DokumenSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/usaha', [SuratUsahaController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('/usaha', [SuratUsahaController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/usaha/{id}', [SuratUsahaController::class, 'show'])->middleware('role:user|make');
+    //     Route::put('/usaha/{id}', [SuratUsahaController::class, 'update'])->middleware('role:user|make');
+    //     Route::delete('/usaha/{id}', [SuratUsahaController::class, 'destroy'])->middleware('role:user|make');
+    //     Route::get('supusaha', [SupportUsahaController::class, 'index'])->middleware('role:user|make');
+    //     Route::get('supusaha/{id}', [SupportUsahaController::class, 'show'])->middleware('role:user|make');
+    //     Route::post('supusaha', [SupportUsahaController::class, 'store'])->middleware('role:user|make');
+    //     Route::put('supusaha/{id}', [SupportUsahaController::class, 'update'])->middleware('role:user|make');
+    //     Route::delete('supusaha/{id}', [SupportUsahaController::class, 'destroy'])->middleware('role:user|make');
+    //     Route::post('/skck', [SkckController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/skck', [SkckController::class, 'index'])->middleware('role:user|make');
+    //     Route::get('/skck/{id}', [SkckController::class, 'show'])->middleware('role:user|make');
+    //     Route::put('/skck/{id}', [SkckController::class, 'update'])->middleware('role:user|make');
+    //     Route::delete('/skck/{id}', [SkckController::class, 'destroy'])->middleware('role:user|make');
+    //     Route::post('/domisili', [DomisiliController::class, 'store'])->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/domisili', [DomisiliController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/domisili/{id}', [DomisiliController::class, 'show'])->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::put('/domisili/{id}', [DomisiliController::class, 'update'])->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::delete('/domisili/{id}', [DomisiliController::class, 'destroy'])->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::post('/sktm', [SKTMController::class, 'store'])->middleware('role:user|make')->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     // Route::resource('/sktm', [SKTMController::class])->middleware('role:user|make');
+    //     Route::get('supSktm', [PendukungSktmController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('supSktm', [PendukungSktmController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('supSktm/{id}', [PendukungSktmController::class, 'show'])->middleware('role:user|make');
+    //     Route::put('supSktm/{id}', [PendukungSktmController::class, 'update'])->middleware('role:user|make');
+    //     Route::delete('supSktm/{id}', [PendukungSktmController::class, 'destroy'])->middleware('role:user|make');
+    //     Route::get('/users', [UsersController::class, 'index'])->middleware('role:user|make');
+    //     Route::put('/users/{user}', [UsersController::class, 'update'])->middleware('role:user|make');
+    //     Route::get('/schedules', [JadwalController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('/schedules/post', [JadwalController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/schedules/{id}', [JadwalController::class, 'show'])->middleware('role:user|make');
+
+    //     Route::get('/submission', [PengajuanController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('/submission/post', [PengajuanController::class, 'store'])->middleware('role:user|make');
+    //     Route::get('/submission/{id}', [PengajuanController::class, 'show'])->middleware('role:user|make');
+
+    //     Route::get('/document', [DokumenController::class, 'index'])->middleware('role:user|make');
+    //     Route::post('/document/post', [DokumenController::class, 'store'])->middleware('role:user|make');
+    // });
